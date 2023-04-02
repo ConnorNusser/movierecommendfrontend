02631a3d067d8eae8f8c5662c0d99cd07f68c046
Connor <connornusser@gmail.com>
2023-04-02 13:36:46 -0500

    login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { useMemo, useState } from 'react';
+import { UserContext } from './hooks/usercontext'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  function setVal(){
+    setValue("New Value");
+  }
+  const [value, setValue] = useState("Hello from context");
+  const providerValue = useMemo(() => ({value, setValue}), [value, setValue]);
+  return <UserContext.Provider value={providerValue}>
+    <Component {...pageProps} />
+    </UserContext.Provider>
 }
